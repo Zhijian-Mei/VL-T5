c0c83f8a6e8ff6d07053e7e12d6019bc80660068
Zhijian Mei <545902915@qq.com>
2023-02-22 11:52:03 +0800

    1

--- get_images_amazon.py
+++ get_images_amazon.py
@@ -1,4 +1,62 @@
 import pandas as pd
 
-df = pd.read_csv('../../FolkScope/TOTAL_typicality_result.csv')
+from tqdm import trange
+
+
+# df = pd.read_csv('../../FolkScope/TOTAL_typicality_result.csv')
+df = pd.read_csv('TOTAL_typicality_result.csv')
 print(df)
+print(df.columns)
+print(df['relation'])
+print(df['assertion'][1])
+itemid_itemimgurl = {}
+for i in trange(len(df)):
+    ida = df['item_a_id'][i]
+    idb = df['item_b_id'][i]
+    if ida not in itemid_itemimgurl:
+        itemid_itemimgurl[ida] = []
+    if idb not in itemid_itemimgurl:
+        itemid_itemimgurl[idb] = []
+    if df['item_a_img1_validity'][i] != 0:
+        url = df['item_a_img1_url'][i]
+        if url not in itemid_itemimgurl[ida]:
+            itemid_itemimgurl[ida].append(url)
+    if df['item_a_img2_validity'][i] != 0:
+        url = df['item_a_img2_url'][i]
+        if url not in itemid_itemimgurl[ida]:
+            itemid_itemimgurl[ida].append(url)
+    if df['item_a_img3_validity'][i] != 0:
+        url = df['item_a_img3_url'][i]
+        if url not in itemid_itemimgurl[ida]:
+            itemid_itemimgurl[ida].append(url)
+
+    if df['item_b_img1_validity'][i] != 0:
+        url = df['item_b_img1_url'][i]
+        if url not in itemid_itemimgurl[idb]:
+            itemid_itemimgurl[idb].append(url)
+    if df['item_b_img2_validity'][i] != 0:
+        url = df['item_b_img2_url'][i]
+        if url not in itemid_itemimgurl[idb]:
+            itemid_itemimgurl[idb].append(url)
+    if df['item_b_img3_validity'][i] != 0:
+        url = df['item_b_img3_url'][i]
+        if url not in itemid_itemimgurl[idb]:
+            itemid_itemimgurl[idb].append(url)
+
+
+print(itemid_itemimgurl)
+
+import requests
+import shutil
+
+for id,url_list in itemid_itemimgurl.items():
+    counter = 1
+    for url in url_list:
+        file_name = wget.download(url)
+        quit()
+        file_name = f'datasets/amazon_imgs/{id}_{counter}.img'
+        f = open(file_name, 'wb')
+        f.write(requests.get(url).content)
+        f.close()
+        counter += 1
+    quit()
